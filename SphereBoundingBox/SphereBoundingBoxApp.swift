cc3385cb718d47464138f9fabe0f0970dfa2a16d
mmcgregor29 <mmcgregor@hawk.iit.edu>
2023-01-13 15:59:17 -0600

    Initial Commit

---
+++ SphereBoundingBox/SphereBoundingBoxApp.swift
@@ -0,0 +1,17 @@
+//
+//  SphereBoundingBoxApp.swift
+//  SphereBoundingBox
+//
+//  Created by IIT Phys 440 on 1/13/23.
+//
+
+import SwiftUI
+
+@main
+struct SphereBoundingBoxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
