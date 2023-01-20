cdfb485f8414dfc2fd53d588d516097b2f49e9a2
mmcgregor29 <mmcgregor@hawk.iit.edu>
2023-01-20 13:25:10 -0600

    FINAL

--- SphereBoundingBoxTests/SphereBoundingBoxTests.swift
+++ SphereBoundingBoxTests/SphereBoundingBoxTests.swift
@@ -6,8 +6,12 @@
 //
 
 import XCTest
+//import SphereBoundingBox2
+//import SphereParameters
+//import BoundingBoxParameters
 
-final class SphereBoundingBoxTests: XCTestCase {
+
+final class SphereBoundingBox2Tests: XCTestCase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
@@ -16,16 +20,39 @@ final class SphereBoundingBoxTests: XCTestCase {
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
-
-    func testExample() throws {
-        
-        
-        
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+///I followed along with your code for the circle test but no matter what I tried I could not get the SphereParemeters and BoundingBoxParameters to be in the scope so I couldn't quite get the testing part to work:(
+        func testExample() throws {
+//
+//                let sphereParameters = SphereParameters()
+//
+//                sphereParameters.sphereRadius = "1.0"
+//                sphereParameters.CalculateSphere()
+//
+//                let CorrectSphereVolume = 4/3 * Double.pi
+//                let CorrectSphereSurfaceArea = 4 * Double.pi
+//
+//                let BBParameters = BoundingBoxParameters
+//
+//                BBParameters.sphereRadius = "1.0"
+//                BBParameters.CalculateSphere()
+//
+//                let CorrectBBVolume = 8.0
+//                let CorrectBBSurfaceArea = 24.0
+//
+//                XCTAssertEqual(CorrectSphereVolume,sphereParameters.sphereVolume, accuracy: 1e-14)
+//
+//                XCTAssertEqual(CorrectSphereSurfaceArea,sphereParameters.sphereSurfaceArea, accuracy: 1e-14)
+//
+//                XCTAssertEqual(CorrectBBVolume,BBParameters.boundingBoxVolume, accuracy: 1e-14)
+//
+//                XCTAssertEqual(CorrectBBSurfaceArea,BBParameters.boundingBoxSurfaceArea, accuracy: 1e-14)
+//
+//
+//         This is an example of a functional test case.
+//         Use XCTAssert and related functions to verify your tests produce the correct results.
+//         Any test you write for XCTest can be annotated as throws and async.
+//         Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
+//         Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
     }
 
     func testPerformanceExample() throws {
